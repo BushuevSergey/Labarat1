525f69adaaaa5963e9c71ac2a6afe5829468f1e9
rudima02 <rudima02@gmail.com>
2025-03-12 08:32:38 +0700

    изменён triangle.cpp

--- triangle.cpp
+++ triangle.cpp
@@ -8,14 +8,18 @@ int main() {
 	double side1, side2, side3;
 	cout << "Введите стороны треугольника: ";
 	cin >> side1 >> side2 >> side3;
+        // Проверка, существует ли треугольник 
 	if (side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1) {
 		cout << "Такого треугольика не существует" << endl;
 		return 1;
 	}
+        // Считаем периметр
 	double trianglePerimetr = side1 + side2 + side3;
+        // Считаем полупериметр для формулы Герона
 	double semiPerimetr = trianglePerimetr / 2;
+        // Считаем площадь по формуле герона
 	double triangleArea = sqrt(semiPerimetr * (semiPerimetr - side1) * (semiPerimetr - side2) * (semiPerimetr - side3));
-
+        // Проверка на равнобедренность
 	double isIsosceles = 0;
 	if (side1 == side2 || side1 == side3 || side2 == side3) {
 		isIsosceles = 1;
@@ -27,7 +31,7 @@ int main() {
 	else {
 		cout << "Треугольник не является равнобедренным." << endl;
 	}
-	
+
 	cout << "Периметр треугольника: " << trianglePerimetr << endl;
 	cout << "Площадь треугольника: " << triangleArea << endl;
 
